45a776880f7230e115d2d16f6aa17cf3cda3fee3
Paul Staszko <paulstaszko@gmail.com>
2024-07-10 16:13:11 -0500

    quick

--- publish.ps1
+++ publish.ps1
@@ -6,12 +6,12 @@ cd $PSScriptRoot
 dotnet pack --include-source
 if($lastexitcode -eq 0){
     $f=dir *.nupkg -rec | sort LastWriteTime -desc | select -first 1
-    ## NOTE: The WCRI_GITHUB_PACKAGES_PASSWORD is a Github classic personal access token with package write priivleges
-    $url = "https://nuget.pkg.github.com/WesternCapital/index.json"
+    ## NOTE: The STARLYCODE_GITHUB_PACKAGES_PASSWORD is a Github classic personal access token with package write priivleges
+    $url = "https://nuget.pkg.github.com/StarlyCode/index.json"
     if($SkipDuplicationCheck){
-        dotnet nuget push $f --source $url --api-key $env:WCRI_GITHUB_PACKAGES_PASSWORD --skip-duplicate
+        dotnet nuget push $f --source $url --api-key $env:STARLYCODE_GITHUB_PACKAGES_PASSWORD --skip-duplicate
     }else{
-        dotnet nuget push $f --source $url --api-key $env:WCRI_GITHUB_PACKAGES_PASSWORD
+        dotnet nuget push $f --source $url --api-key $env:STARLYCODE_GITHUB_PACKAGES_PASSWORD
     }
     if($LASTEXITCODE -eq 0){
         Write-Host "Successfully pushed package to Github Packages"
